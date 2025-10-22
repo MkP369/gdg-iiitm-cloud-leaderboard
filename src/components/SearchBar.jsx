b52f046c2cdcb8842f8344e216595638c772f300
Kritarth Prasad <kritarthprasad01@gmail.com>
2025-10-22 11:54:02 +0530

    updated for 22/10

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -31,7 +31,6 @@ export default function SearchBar({ onSearch }) {
             />
           </svg>
         </div>
-
         <input
           type="text"
           placeholder="Search for a participant..."
